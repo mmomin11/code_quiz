ef4d8f368abf42e90f53a96b9ed963c8518bd664
New Name <mubin.momin@nxp.com>
2020-05-13 17:16:50 -0500

    declared vars

--- highscore.js
+++ highscore.js
@@ -1,4 +1,7 @@
 // decalred variables
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");
 
 // event listener to clear scores
 
